d763d44a927e43be7a4ec30a58c630be1d83e0c2
IrishCinnamon <18431742+IrishGitlord1@users.noreply.github.com>
2024-02-08 14:51:10 -0500

    First Item

--- src/main/java/net/lamecinnamon/epistemikamod/EpistemikaMod.java
+++ src/main/java/net/lamecinnamon/epistemikamod/EpistemikaMod.java
@@ -1,15 +1,15 @@
-package net.lamecinnamon.epistemika;
+package net.lamecinnamon.epistemikamod;
 
 import com.mojang.logging.LogUtils;
+import net.lamecinnamon.epistemikamod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -18,13 +18,13 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(EpistemikaMod.MOD_ID)
 public class EpistemikaMod {
-    // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "epistemika";
-    // Directly reference a slf4j logger
+    public static final String MOD_ID = "epistemikamod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public EpistemikaMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -38,7 +38,9 @@ public class EpistemikaMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.IOLITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
